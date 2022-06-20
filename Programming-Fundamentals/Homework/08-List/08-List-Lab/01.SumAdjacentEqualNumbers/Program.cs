8eaa6157d1a4c8cf5700ceb25258a46447fdaa1c
Viktor Pazhev <asusvikp@gmail.com>
2022-06-20 10:28:52 +0300

    Started List Lab

---
+++ Programming-Fundamentals/Homework/08-List/08-List-Lab/01.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SumAdjacentEqualNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    numbers[i - 1] *= 2;
+                    numbers.RemoveAt(i);
+                    i = Math.Max(0, i - 2);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ Programming-Fundamentals/Homework/08-List/08-List-Lab/03.MergingLists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MergingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> list2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> newList = new List<int>();
+            bool fromList1 = true;
+            while (Math.Min(list1.Count, list2.Count) > 0)
+            {
+                if (fromList1)
+                {
+                    newList.Add(list1[0]);
+                    list1.RemoveAt(0);
+                }
+                else
+                {
+                    newList.Add(list2[0]);
+                    list2.RemoveAt(0);
+                }
+                fromList1 = !fromList1;
+            }
+
+            newList.AddRange(list1);
+            newList.AddRange(list2);
+
+            Console.WriteLine(string.Join(" ", newList));
+        }
+    }
+}
