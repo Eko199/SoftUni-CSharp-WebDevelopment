72f6fb3cdb218c176a2c86e491f06a3da802d5a8
Viktor Pazhev <asusvikp@gmail.com>
2022-06-26 10:08:14 +0300

    Added Mid Exam Preparation

---
+++ Programming-Fundamentals/Exams/Mid-Exam-Preparation/02.TheLift/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.TheLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int WagonCapacity = 4;
+            int passengers = int.Parse(Console.ReadLine());
+            int[] lift = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (var i = 0; i < lift.Length && passengers > 0; i++)
+            {
+                while (lift[i] < WagonCapacity && passengers > 0)
+                {
+                    lift[i]++;
+                    passengers--;
+                }
+            }
+
+            if (passengers > 0)
+                Console.WriteLine($"There isn't enough space! {passengers} people in a queue!");
+            else if (lift.Sum() < WagonCapacity * lift.Length)
+                Console.WriteLine("The lift has empty spots!");
+
+            Console.WriteLine(string.Join(' ', lift));
+        }
+    }
+}
---
+++ Programming-Fundamentals/Exams/Mid-Exam-Preparation/03.MovingTarget/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MovingTarget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targets = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+
+            while (!command.Equals("End"))
+            {
+                string[] tokens = command.Split();
+                int index = int.Parse(tokens[1]);
+                int parameter = int.Parse(tokens[2]);
+
+                switch (tokens[0])
+                {
+                    case "Shoot":
+                        if (ValidIndex(index, targets))
+                        {
+                            targets[index] -= parameter;
+                            if (targets[index] <= 0)
+                                targets.RemoveAt(index);
+                        }
+
+                        break;
+
+                    case "Add":
+                        if (!ValidIndex(index, targets))
+                            Console.WriteLine("Invalid placement!");
+                        else
+                            targets.Insert(index, parameter);
+                        break;
+
+                    case "Strike":
+                        if (!ValidIndex(index - parameter, targets) || !ValidIndex(index + parameter, targets))
+                            Console.WriteLine("Strike missed!");
+                        else
+                        {
+                            int targetsToRemove = parameter * 2 + 1;
+                            for (int i = 0; i < targetsToRemove; i++)
+                                targets.RemoveAt(index - parameter);
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join('|', targets));
+        }
+
+        private static bool ValidIndex(int index, ICollection<int> collection) => index >= 0 && index < collection.Count;
+    }
+}
