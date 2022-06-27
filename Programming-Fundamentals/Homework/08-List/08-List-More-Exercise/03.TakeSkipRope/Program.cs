584d9e0065506dd66c77c2db99d53747f6755fd4
Viktor Pazhev <asusvikp@gmail.com>
2022-06-27 10:22:24 +0300

    Added List More Exercise 03

---
+++ Programming-Fundamentals/Homework/08-List/08-List-More-Exercise/03.TakeSkipRope/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.TakeSkipRope
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> stringList = Console.ReadLine().ToList();
+            List<int> takeList = stringList.Where(c => char.IsDigit(c)).Select(c => c - '0').ToList();
+            stringList.RemoveAll(c => char.IsDigit(c));
+
+            List<int> skipList = takeList.Where((x, index) => index % 2 == 1).ToList();
+            takeList = takeList.Where((x, index) => index % 2 == 0).ToList();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < skipList.Count; i++)
+            {
+                stringBuilder.Append(string.Join("", stringList.Take(takeList[i])));
+                stringList.RemoveRange(0, Math.Min(skipList[i] + takeList[i], stringList.Count));
+            }
+
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
