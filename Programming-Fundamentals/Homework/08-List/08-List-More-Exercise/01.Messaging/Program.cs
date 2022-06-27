68f8947f35fb6a74d56dc461f1fe85ab755a3e22
Viktor Pazhev <asusvikp@gmail.com>
2022-06-27 07:00:37 +0300

    Started List More Exercise

---
+++ Programming-Fundamentals/Homework/08-List/08-List-More-Exercise/01.Messaging/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.Messaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string message = Console.ReadLine();
+
+            StringBuilder result = new StringBuilder();
+            foreach (int number in numbers)
+            {
+                int current = number, digitSum = 0;
+                while (current > 0)
+                {
+                    digitSum += current % 10;
+                    current /= 10;
+                }
+
+                digitSum %= message.Length;
+                result.Append(message[digitSum]);
+                message = message.Remove(digitSum, 1);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Programming-Fundamentals/Homework/08-List/08-List-More-Exercise/02.CarRace/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.CarRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> times = Console.ReadLine().Split().Select(int.Parse).ToList();
+            decimal leftTime = 0, rightTime = 0;
+
+            for (int i = 0; i < times.Count / 2; i++)
+            {
+                if (times[i] == 0)
+                    leftTime -= leftTime / 5;
+                else
+                    leftTime += times[i];
+
+                if (times[^(i + 1)] == 0)
+                    rightTime -= rightTime / 5;
+                else
+                    rightTime += times[^(i + 1)];
+            }
+
+            Console.WriteLine("The winner is {0} with total time: {1}", leftTime > rightTime ? "right" : "left", Math.Min(leftTime, rightTime));
+        }
+    }
+}
