4617a78983b23a5b69339421186eba53043599e4
Viktor Pazhev <asusvikp@gmail.com>
2022-06-27 16:48:55 +0300

    Finished List More Exercise

---
+++ Programming-Fundamentals/Homework/08-List/MoreExercise/04.MixedUpLists/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.MixedUpLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> list2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+
+            int lesserCount = Math.Min(list1.Count, list2.Count);
+            for (int i = 0; i < lesserCount; i++)
+            {
+                result.Add(list1[0]);
+                list1.RemoveAt(0);
+                result.Add(list2[^1]);
+                list2.RemoveAt(list2.Count - 1);
+            }
+
+            int lowerBound, upperBound;
+            if (list1.Count > 0)
+            {
+                lowerBound = Math.Min(list1[0], list1[1]);
+                upperBound = Math.Max(list1[0], list1[1]);
+            }
+            else
+            {
+                lowerBound = Math.Min(list2[0], list2[1]);
+                upperBound = Math.Max(list2[0], list2[1]);
+            }
+
+            result.RemoveAll(num => num <= lowerBound || num >= upperBound);
+            result.Sort();
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ Programming-Fundamentals/Homework/08-List/MoreExercise/05.DrumSet/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.DrumSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+            List<int> initialQualities = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> qualities = new List<int>(initialQualities);
+
+            string input = Console.ReadLine();
+            while (!input.Equals("Hit it again, Gabsy!"))
+            {
+                int power = int.Parse(input);
+
+                for (int i = 0; i < qualities.Count; i++)
+                {
+                    if (qualities[i] <= 0) continue;
+
+                    qualities[i] -= power;
+                    if (qualities[i] > 0) continue;
+
+                    double price = initialQualities[i] * 3;
+                    if (savings >= price)
+                    {
+                        qualities[i] = initialQualities[i];
+                        savings -= price;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            qualities.RemoveAll(drum => drum <= 0);
+            Console.WriteLine(string.Join(' ', qualities));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}
