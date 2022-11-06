1f524cb45b6b443cf18d9820cdaaa9bce4c73731
Viktor Pazhev <asusvikp@gmail.com>
2022-11-06 17:35:13 +0200

    Added Inheritance Lab

---
+++ C#-OOP/Homework/01-Inheritance/Lab/CustomRandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+            int rndIndex = random.Next(0, Count);
+
+            string randomString = this[rndIndex];
+            RemoveAt(rndIndex);
+
+            return randomString;
+        }
+    }
+}
---
+++ C#-OOP/Homework/01-Inheritance/Lab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => Count == 0;
+
+        public Stack<string> AddRange(ICollection<string> range)
+        {
+            foreach (string str in range)
+            {
+                Push(str);
+            }
+
+            return this;
+        }
+    }
+}
