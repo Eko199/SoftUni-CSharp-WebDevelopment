f487fa2fbef65f8f551eeccb88dbf1c16f015ba7
Viktor Pazhev <asusvikp@gmail.com>
2024-01-31 00:07:44 +0200

    Add Data Structures Augmentation Lab

---
+++ Data-Structures-Advanced/Homework/04-Data-Structures-Augmentation-Lab/PeopleCollection/PeopleCollection.cs
@@ -0,0 +1,81 @@
+﻿namespace CollectionOfPeople
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Wintellect.PowerCollections;
+
+    public class PeopleCollection : IPeopleCollection
+    {
+        private readonly IDictionary<string, Person> peopleByEmail = new Dictionary<string, Person>();
+        private readonly IDictionary<string, SortedSet<Person>> peopleByEmaiDomain = new Dictionary<string, SortedSet<Person>>();
+
+        private readonly IDictionary<(string name, string town), SortedSet<Person>> peopleByNameAndTown =
+            new Dictionary<(string, string), SortedSet<Person>>();
+
+        private readonly OrderedDictionary<int, SortedSet<Person>> peopleByAge = new OrderedDictionary<int, SortedSet<Person>>();
+
+        private readonly IDictionary<string, OrderedDictionary<int, SortedSet<Person>>> peopleByTownAndAge =
+            new Dictionary<string, OrderedDictionary<int, SortedSet<Person>>>();
+
+        public int Count => peopleByEmail.Count;
+
+        public bool Add(string email, string name, int age, string town)
+        {
+            if (Find(email) != null)
+                return false;
+
+            var person = new Person(email, name, age, town);
+
+            peopleByEmail.Add(email, person);
+            peopleByEmaiDomain.AppendValueToKey(email.Split('@')[1], person);
+            peopleByNameAndTown.AppendValueToKey((name, town), person);
+            peopleByAge.AppendValueToKey(age, person);
+
+            peopleByTownAndAge.EnsureKeyExists(town);
+            peopleByTownAndAge[town].AppendValueToKey(age, person);
+
+            return true;
+        }
+
+        public bool Delete(string email)
+        {
+            var person = Find(email);
+
+            if (person == null)
+            {
+                return false;
+            }
+
+            peopleByEmaiDomain[email.Split('@')[1]].Remove(person);
+            peopleByNameAndTown[(person.Name, person.Town)].Remove(person);
+            peopleByAge[person.Age].Remove(person);
+            peopleByTownAndAge[person.Town][person.Age].Remove(person);
+
+            return peopleByEmail.Remove(email);
+        }
+
+        public Person Find(string email) => !peopleByEmail.ContainsKey(email) ? null : peopleByEmail[email];
+
+        public IEnumerable<Person> FindPeople(string emailDomain) => peopleByEmaiDomain.GetValuesForKey(emailDomain);
+
+        public IEnumerable<Person> FindPeople(string name, string town) => peopleByNameAndTown.GetValuesForKey((name, town));
+
+        public IEnumerable<Person> FindPeople(int startAge, int endAge)
+            => peopleByAge.Range(startAge, true, endAge, true)
+                .SelectMany(agePeople => agePeople.Value)
+                .OrderBy(p => p.Age)
+                .ThenBy(p => p.Email);
+
+        public IEnumerable<Person> FindPeople(int startAge, int endAge, string town)
+        {
+            if (!peopleByTownAndAge.ContainsKey(town))
+            {
+                return Enumerable.Empty<Person>();
+            }
+
+            return peopleByTownAndAge[town].Range(startAge, true, endAge, true)
+                .SelectMany(agePeople => agePeople.Value);
+        }
+    }
+}
---
+++ Data-Structures-Advanced/Homework/04-Data-Structures-Augmentation-Lab/PeopleCollection/PeopleCollectionSlow.cs
@@ -0,0 +1,47 @@
+﻿namespace CollectionOfPeople
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PeopleCollectionSlow : IPeopleCollection
+    {
+        private readonly IList<Person> people = new List<Person>();
+
+        public int Count => people.Count;
+
+        public bool Add(string email, string name, int age, string town)
+        {
+            if (Find(email) != null)
+            {
+                return false;
+            }
+
+            people.Add(new Person(email, name, age, town));
+            return true;
+        }
+
+        public bool Delete(string email) => people.Remove(Find(email));
+
+        public Person Find(string email) => people.SingleOrDefault(p => p.Email == email);
+
+        public IEnumerable<Person> FindPeople(string emailDomain)
+            => people.Where(p => p.Email.EndsWith($"@{emailDomain}"))
+                .OrderBy(p => p.Email);
+
+        public IEnumerable<Person> FindPeople(string name, string town)
+            => people.Where(p => p.Name == name && p.Town == town)
+                .OrderBy(p => p.Email);
+
+        public IEnumerable<Person> FindPeople(int startAge, int endAge)
+            => people.Where(p => p.Age >= startAge && p.Age <= endAge)
+                .OrderBy(p => p.Age)
+                .ThenBy(p => p.Email);
+
+        public IEnumerable<Person> FindPeople(int startAge, int endAge, string town)
+            => FindPeople(startAge, endAge)
+                .Where(p => p.Town == town)
+                .OrderBy(p => p.Age)
+                .ThenByDescending(p => p.Town)
+                .ThenBy(p => p.Email);
+    }
+}
